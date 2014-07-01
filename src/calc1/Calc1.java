f5d880f311c9e939d502ece95fa1f52aa6ed701f
pavel <pmarkhotin@yandex.ru>
2014-07-02 03:35:21 +0400

    проверка фиксации

---
+++ src/calc1/Calc1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calc1;
+
+/**
+ *
+ * @author pavel
+ */
+public class Calc1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
